87b3d49d71436cbd3e59f7b5b1e640c9a4a39daa
PalmyreB <palmyre.baroth@live.fr>
2019-05-06 18:28:50 -0400

    Code smell "Assuming Safe Multi-Language Return Values"

---
+++ CodeSmellsC/AssumingSelfMultiLanguageReturnValues.c
@@ -0,0 +1,19 @@
+/*
+ * Source:
+ * https://www3.ntu.edu.sg/home/ehchua/programming/java/JavaNativeInterface.html
+ */
+
+#include <jni.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "..\CodeSmellsJNI\bin\codeSmellsJava_AssumingSelfMultiLanguageReturnValues.h"
+
+JNIEXPORT void JNICALL Java_codeSmellsJava_AssumingSelfMultiLanguageReturnValues_callMethodOutputFromClass(
+		JNIEnv *env, jobject obj) {
+	jclass clazz = (*env)->FindClass(env,
+			"codeSmellsJava/AssumingSelfMultiLanguageReturnValues");
+	jmethodID method = (*env)->GetStaticMethodID(env, clazz, "output",
+			"()V");
+	(*env)->CallStaticVoidMethod(env, clazz, method, NULL);
+}
---
+++ CodeSmellsJNI/src/codeSmellsJava/AssumingSelfMultiLanguageReturnValues.java
@@ -0,0 +1,27 @@
+package codeSmellsJava;
+
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+
+public class AssumingSelfMultiLanguageReturnValues {
+	static {
+		AccessController.doPrivileged(new PrivilegedAction<Void>() {
+			public Void run() {
+				System.loadLibrary("JNILIB"); // Code smell "Not using relative path"
+				return null;
+			}
+		});
+	}
+
+	public static void output() {
+		System.out.println("Output from class");
+	}
+
+	public native void callMethodOutputFromClass();
+
+	public static void main(String args[]) {
+		AssumingSelfMultiLanguageReturnValues test = new AssumingSelfMultiLanguageReturnValues();
+		System.out.println("*********** Code Smell: Assuming Safe Multi-Language Return Values ***************");
+		test.callMethodOutputFromClass();
+	}
+}
