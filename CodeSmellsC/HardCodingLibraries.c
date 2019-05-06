fa6fd042200322d10243108b18689a66d2203528
PalmyreB <palmyre.baroth@live.fr>
2019-05-06 12:40:51 -0400

    Code smell "Hard Coding Libraries"

---
+++ CodeSmellsC/HardCodingLibraries.c
@@ -0,0 +1,9 @@
+#include <jni.h>
+#include <stdio.h>
+
+#include "..\CodeSmellsJNI\bin\codeSmellsJava_HardCodingLibraries.h"
+
+jint JNICALL Java_codeSmellsJava_HardCodingLibraries_distance(JNIEnv *env,
+		jobject thisObject, jint time, jint speed) {
+	return time * speed;
+}
---
+++ CodeSmellsJNI/src/codeSmellsJava/HardCodingLibraries.java
@@ -0,0 +1,31 @@
+package codeSmellsJava;
+
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+
+public class HardCodingLibraries {
+
+	static {
+		AccessController.doPrivileged(new PrivilegedAction<Void>() {
+			public Void run() {
+				try {
+					System.loadLibrary("JNILIB");
+				} catch (UnsatisfiedLinkError e1) {
+					try {
+						System.loadLibrary("JNILIB"); // some other library
+					} catch (UnsatisfiedLinkError e2) {
+					}
+				}
+				return null;
+			}
+		});
+	}
+
+	public native int distance(int time, int speed);
+
+	public static void main(String args[]) {
+		HardCodingLibraries test = new HardCodingLibraries();
+
+		System.out.println("The distance is " + test.distance(3, 2));
+	}
+}
